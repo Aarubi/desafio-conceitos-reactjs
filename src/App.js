fac261a781fc6ab7856327c7a4f3ef9d1635efa4
Arthur Leitão <arthurleitaobarbosa@hotmail.com>
2020-06-28 19:18:03 -0300

    Primeiro desafio de ReactJS

--- src/App.js
+++ src/App.js
@@ -1,31 +1,54 @@
-import React from "react";
+  import React, { useState, useEffect } from "react";
 
-import "./styles.css";
+  import "./styles.css";
+  import api from "./services/api";
 
-function App() {
-  async function handleAddRepository() {
-    // TODO
+  function App() {
+
+    const [repositories, setRepositories] =useState([]);
+
+    useEffect(() => {
+      api.get('/repositories').then(response => {
+        setRepositories(response.data);
+      });
+    }, []);
+
+    async function handleAddRepository() {
+      
+    const response = await api.post('/repositories', {
+        title:`Sr. Leitão è o Deus dos Animes.`,
+        url: "Niconimes.com.br",
+        techs: ["Javascript", "Node.js", "React"]
+      });
+
+      const repository= response.data;
+      setRepositories([...repositories, repository]);
+    }
+
+    async function handleRemoveRepository(id) {
+      await api.delete(`repositories/${id}`);
+  
+      setRepositories(repositories.filter(repository => repository.id !== id));
+  
+    }
+
+    return (
+        
+      <div>
+        <ul data-testid="repository-list">
+          {repositories.map(repository =>( 
+          <li key={repository.id}> {repository.title} 
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>)
+          )}
+           
+        </ul>
+
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
+    );
   }
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
-
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
-}
-
-export default App;
+  export default App;
